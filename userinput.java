bf24e5301b18a740b61ebf1991d438143461114c
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-13 21:53:25 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,69 @@
+package com.day2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class userinput
+ */
+@WebServlet("/userinput")
+public class userinput extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public userinput() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		
+		response.setContentType("text/html");
+		PrintWriter write = response.getWriter();
+		String FirstName = request.getParameter("FirstName");
+		String LastName = request.getParameter("LastName");
+		String StreetAddress = request.getParameter("StreetAddress");
+		write.print("<h5> First Name : </h5>" + FirstName + "<h1>");
+		write.print("<h5>Last Name : </h5>" + LastName + "<h1>");
+		write.print("<h3>Street Address : </h3>" + StreetAddress + "<h1>");
+		
+		
+	}
+
+}
