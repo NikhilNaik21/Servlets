3127323ed3ff19089b0bd08e2af67858a3345d87
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-13 22:06:05 +0530

    Update userinput.java

--- userinput.java
+++ userinput.java
@@ -53,7 +53,7 @@ public class userinput extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		doGet(request, response);
-		
+		-- 
 		response.setContentType("text/html");
 		PrintWriter write = response.getWriter();
 		String FirstName = request.getParameter("FirstName");
